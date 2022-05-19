4e738999fb52f16ea0b0cbb362559488b5f3eb00
Mark Romero <Mtovar0@hotmail.com>
2022-05-18 20:00:27 -0500

    Commit #4 (Correcciones)

--- Bank Marketing/bank-fulll.py
+++ Bank Marketing/bank-fulll.py
@@ -44,3 +44,69 @@ test_out_y = np.array(test_data.y)
 
 #  Algoritmos
 
+logreg = LogisticRegression(solver='lbfgs', max_iter = 7600)
+
+logreg.fit(train_x, train_y)
+
+
+
+
+print('*'*50)
+
+print('regresión logística')
+
+print(f'accuracy de Entrenamiento de Entrenamiento: {logreg.score(train_x, train_y)}')
+
+print(f'accuracy de Test de Entrenamiento: {logreg.score(test_x, test_y)}')
+
+print(f'accuracy de Validación: {logreg.score(test_out_x, test_out_y)}')
+
+
+svc = SVC(gamma='auto')
+
+svc.fit(train_x, train_y)
+
+
+print('*'*50)
+
+print('maquina de soporte vectorial')
+
+print(f'accuracy de Entrenamiento de Entrenamiento: {svc.score(train_x, train_y)}')
+
+print(f'accuracy de Test de Entrenamiento: {svc.score(test_x, test_y)}')
+
+print(f'accuracy de Validación: {svc.score(test_out_x, test_out_y)}')
+
+
+random_arbol = RandomForestClassifier()
+
+
+random_arbol.fit(train_x, train_y)
+
+
+print('*'*50)
+
+print('arbol random')
+
+print(f'accuracy de Entrenamiento de Entrenamiento: {random_arbol.score(train_x, train_y)}')
+
+print(f'accuracy de Test de Entrenamiento: {random_arbol.score(test_x, test_y)}')
+
+print(f'accuracy de Validación: {random_arbol.score(test_out_x, test_out_y)}')
+
+
+arbol = DecisionTreeClassifier()
+
+
+arbol.fit(train_x, train_y)
+
+
+print('*'*50)
+
+print('arbol desicion')
+
+print(f'accuracy de Entrenamiento de Entrenamiento: {arbol.score(train_x, train_y)}')
+
+print(f'accuracy de Test de Entrenamiento: {arbol.score(test_x, test_y)}')
+
+print(f'accuracy de Validación: {arbol.score(test_out_x, test_out_y)}')
--- read.md
+++ read.md
@@ -1,3 +1,4 @@
 Commit #1 (Commit inicial uso el proyecto anterior como base)
 Commit #2 (Creacion de la rama develop)
 Commit #3 (Ajustes en el proyecto anterior)
+Commit #4 (Correcciones)
